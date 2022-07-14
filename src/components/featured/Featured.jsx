c39f149b8556fcfeaeadc0920c43d597bdf98785
osmanforhad <developer.osmanforhad@gmail.com>
2022-07-14 23:16:16 +0600

    fetch data from database using custom hook

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import useFetch from '../../hooks/useFetch';
 import './featured.css';
 
 const Featured = () => {
+  const apiEndPoint = "http://localhost:5000/api/hotels/countByCity?cities=berlin,madrid,london";
+  const {data, loading, error} = useFetch(apiEndPoint);
+
   return (
 <div className="featured">
-      <div className="featuredItem">
+     {loading ? (
+      "Loadign please wait" 
+     ) : (<> <div className="featuredItem">
         <img
           src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
           alt=""
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Dublin</h1>
-          <h2>123 properties</h2>
+          <h1>Berlin</h1>
+          <h2>{data[0]} properties</h2>
         </div>
       </div>
       
@@ -23,10 +29,11 @@ const Featured = () => {
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Reno</h1>
-          <h2>533 properties</h2>
+          <h1>Madrid</h1>
+          <h2>{data[1]} properties</h2>
         </div>
       </div>
+
       <div className="featuredItem">
         <img
           src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
@@ -34,10 +41,11 @@ const Featured = () => {
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Austin</h1>
-          <h2>532 properties</h2>
+          <h1>London</h1>
+          <h2>{data[2]} properties</h2>
         </div>
-      </div>
+      </div></>)}
+
     </div>
   )
 }
